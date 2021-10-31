3cbfc1e2b4cca5d9a65285599229853262183d18
soosungp33 <soosungp33@gmail.com>
2021-11-01 00:38:52 +0900

    init: backend 초기 설정

---
+++ backend/src/user/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column()
+  user_image: string;
+
+  @Column()
+  user_nickname: string;
+
+  @Column()
+  user_email: string;
+}
