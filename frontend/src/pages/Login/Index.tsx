743c9e0ae7bfa9f2225513a1f094abfc32bd8d8d
Jo-Myounghee <jomyounghee32@gmail.com>
2021-11-02 02:53:42 +0900

    Feat: 로그인 페이지 UI 생성
    피그마에 있는 로그인 페이지와 동일한 UI 생성
    
    #13

--- frontend/src/pages/Login/Index.tsx
+++ frontend/src/pages/Login/Index.tsx
@@ -1,7 +1,132 @@
 import React from "react";
+import styled from "styled-components";
 
-function Login() {
-  return;
-}
+const BackgroundImg = styled.div<any>`
+  background-image: url(${props => props.imgUrl});
+  background-size: cover;
+  height: 100vh;
+`;
+
+const BackgroundWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #ffffff90;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 500px;
+  width: 80vw;
+  margin: 0 auto;
+`;
+
+const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 300px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const LogoText = styled.h1`
+  font-size: 30px;
+  font-weight: bold;
+  line-height: 50px;
+  margin-left: 10px;
+`;
+
+const LogoInfoText = styled.p`
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+const Polaroid = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 340px;
+  height: 500px;
+  border-radius: 15px;
+  background-color: white;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.25);
+  & > img {
+    padding-top: 35px;
+  }
+`;
+
+const Footer = styled.div`
+  position: absolute;
+  bottom: 0px;
+  text-align: center;
+  width: 100vw;
+`;
+
+const FooterContent = styled.div`
+  margin: 0 auto;
+  padding: 20px;
+  width: 80vw;
+  border-top: 2px solid black;
+  & > p {
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+  & > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    & .names {
+      color: #0202029b;
+      padding-left: 10px;
+    }
+  }
+`;
+
+const Login = () => {
+  return (
+    <>
+      <BackgroundImg imgUrl="/img/sand.jpg">
+        <BackgroundWrap>
+          <Content>
+            <LeftSide>
+              <div>
+                <Logo>
+                  <img src="/img/podo.png" alt="justus logo" height="50px" />
+                  <LogoText>우리끼리</LogoText>
+                </Logo>
+                <LogoInfoText>흩어진 추억을 한 곳에 기록해보세요</LogoInfoText>
+              </div>
+              <img src="/img/btnG_완성형.png" alt="" width="350px" />
+            </LeftSide>
+            <div id="right-side">
+              <Polaroid id="polaroid">
+                <img src="/img/glass.jpg" width="300px" height="300px" alt="" />
+                <img src="/img/life_is_travel.png" height="40px" alt="" />
+              </Polaroid>
+            </div>
+          </Content>
+          <Footer>
+            <FooterContent>
+              <p>Copyright ⓒ Team UNDEFINED. 모든 권리 보유.</p>
+              <div>
+                <span>우리끼리</span>
+                <img src="/img/github_icon.png" height="18px" alt="github icon" />
+                <span className="names">공필상 | 김무성 | 김영한 | 조명희</span>
+              </div>
+            </FooterContent>
+          </Footer>
+        </BackgroundWrap>
+      </BackgroundImg>
+    </>
+  );
+};
 
 export default Login;
