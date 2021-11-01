673cff2cf094de054c3feb4ef051a1cbd86a2075
soosungp33 <soosungp33@gmail.com>
2021-11-02 01:22:39 +0900

    CI: Docker-Compose 작성 - react, node, nginx, mysql을 하나의 네트워크로 묶음 - node mysql 설정을 컨테이너 환경 변수로 설정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,21 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { getConnectionOptions } from 'typeorm';
-import { UserModule } from './user/user.module';
-
-TypeOrmModule.forRootAsync({
-  useFactory: async () =>
-    Object.assign(await getConnectionOptions(), {
-      autoLoadEntities: true,
-    }),
-});
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      synchronize: true, // false로 해두는 게 안전하다.
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
