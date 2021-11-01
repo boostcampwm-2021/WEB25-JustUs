298f123d8701c055bff61749b48030550fbff3d3
김무성 <ddb8036631@gmail.com>
2021-11-02 01:00:15 +0900

    Feat: 사이드바 depth2 구현
    포스트 클릭마다 배경과 글씨 색을 변경해야 하므로, 클릭시마다 리렌더링이 필요하다고
    생각했음. 그래서 AlbumList/index.tsx에서 state로 관리하자고 생각했음.
    
    #34

---
+++ frontend/src/components/Sidebar/SecondDepth/SettingGroup/index.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import Color from "@components/Styles/Color";
+
+const SettingGroup = () => {
+  return (
+    <SettingGroupWrapper>
+      <div>그룹 A</div>
+      <div>
+        <img src="/icons/settings.svg" alt="settings.svg" />
+      </div>
+    </SettingGroupWrapper>
+  );
+};
+
+const SettingGroupWrapper = styled.div`
+  width: 15vw;
+  height: 6vh;
+  background-color: ${Color["theme1-primary"]};
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-bottom: 1px ${Color.white} solid;
+  color: ${Color.white};
+`;
+
+export default SettingGroup;
