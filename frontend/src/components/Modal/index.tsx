a365ff3be1bfb7c50cf19e0f7a8604a7d06235e3
gofeel8 <gofeel8@hanmail.net>
2021-11-02 02:40:24 +0900

    Feat: modal css  변경
    display flex 속성과 화면 가득 채우도록 css 수정
    
    #37

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -2,6 +2,17 @@ import React, { ReactNode, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const modalRootEl = document.getElementById("modal");
+if (modalRootEl) {
+  modalRootEl.style.display = "flex";
+  modalRootEl.style.justifyContent = "center";
+  modalRootEl.style.alignItems = "center";
+  modalRootEl.style.position = "absolute";
+  modalRootEl.style.top = "0";
+  modalRootEl.style.left = "0";
+  modalRootEl.style.width = "100%";
+  modalRootEl.style.height = "100%";
+  modalRootEl.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+}
 
 interface ModalProps {
   children: ReactNode;
