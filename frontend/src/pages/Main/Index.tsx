e7715554ad7d5fa51d2436c28d7f6598ef92c9d0
gofeel8 <gofeel8@hanmail.net>
2021-11-02 13:19:47 +0900

    Feat: Modal 수정
    모달 버튼 위치 및 배경 색상 설정
    
    #37

--- frontend/src/pages/Main/Index.tsx
+++ frontend/src/pages/Main/Index.tsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
 import Sidebar from "@components/Sidebar";
+import ModalManager from "@components/Modal/ModalManager";
 
 const Main = () => {
+  const [modalOpen, setModal] = useState("");
   const [isToggle, setIsToggle] = useState<boolean>(true);
+
+  const openModal = (event: React.SyntheticEvent<EventTarget>) => {
+    if (!(event.target instanceof HTMLButtonElement)) return;
+    event.preventDefault();
+    const {
+      target: {
+        dataset: { modal },
+      },
+    } = event;
+    if (modal) setModal(modal);
+  };
+
+  const closeModal = () => {
+    setModal("");
+  };
 
   return (
     <>
       <Sidebar isToggle={isToggle} setIsToggle={setIsToggle} />
+      <button onClick={openModal} data-modal="PostCreateModal">
+        +
+      </button>
+      <ModalManager closeFn={closeModal} modal={modalOpen} />
     </>
   );
 };
