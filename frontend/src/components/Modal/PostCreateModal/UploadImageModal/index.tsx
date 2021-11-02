720032fe63d6e866bc34d51b9dd159201b395a98
gofeel8 <gofeel8@hanmail.net>
2021-11-02 15:50:52 +0900

    Feat: UploadImageModal 헤더 타이틀 변경 및 닫기 버튼 추가
    사진 입력받는 모달창 수정
    
    #5

--- frontend/src/components/Modal/PostCreateModal/UploadImageModal/index.tsx
+++ frontend/src/components/Modal/PostCreateModal/UploadImageModal/index.tsx
@@ -11,10 +11,12 @@ const UploadImageModal = ({ closeFn }: UploadImageModalProps) => {
         event.nativeEvent.stopImmediatePropagation();
       }}
     >
-      <div>모달창</div>
-      <button type="button" onClick={closeFn}>
-        x
-      </button>
+      <ModalHeader>
+        <ModalTitle>새 게시물 만들기</ModalTitle>
+        <ModalClose type="button" onClick={closeFn}>
+          <img src="/icons/x.svg" alt="close" height="60%"></img>
+        </ModalClose>
+      </ModalHeader>
     </ModalContainer>
   );
 };
@@ -25,7 +27,39 @@ const ModalContainer = styled.div`
   background-color: #ffffff;
   height: 30vw;
   width: 45vw;
-  padding: 2vw;
   border-radius: 10px;
   box-shadow: 0 2px 3px 0 rgba(34, 36, 38, 0.15);
+`;
+
+const ModalHeader = styled.div`
+  display: grid;
+  grid-template-columns: 10% 80% 10%;
+  padding: 1vw;
+  height: 2.5vw;
+  border-bottom: 1px solid black;
+  font-size: 1.2vw;
+`;
+
+const ModalTitle = styled.div`
+  grid-column-start: 2;
+  grid-column-end: 3;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalClose = styled.button`
+  grid-column-start: 3;
+  grid-column-end: 4;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  right: 0px;
+  top: 0;
+  padding: 0;
+  border: none;
+  background: none;
+  cursor: pointer;
 `;
