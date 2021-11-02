0f4c9576d5cfd72b60a9d92ddcd660bdc7a0e417
ddb8036631 <50266862+ddb8036631@users.noreply.github.com>
2021-11-02 11:31:26 +0900

    사이드바 디자인 구현 (#35)
    * Feat: 사이드바 depth1 구현
    
    useState가 리턴하는 setter 함수(setIsToggle)의 타입이
    React.Dispatch<React.SetStateAction<boolean>>이어서 setIsToggle의 타입을
    Dispatch<SetStateAction<boolean>>로 설정했다.
    
    #34
    
    * Feat: GlobalStyle 적용
    
    Sidebar 테스트를 위해 간단히 margin과 padding만 초기화했다.
    
    #34
    
    * Fix: project path에 중간 경로 frontend 제거
    
    eslint 오류로 /frontend/frontend/tsconfig.json이라는 경로로 잡아서 해당
    부분을 변경함.
    
    #34
    
    * Feat: 사이드바 depth2 구현
    
    포스트 클릭마다 배경과 글씨 색을 변경해야 하므로, 클릭시마다 리렌더링이 필요하다고
    생각했음. 그래서 AlbumList/index.tsx에서 state로 관리하자고 생각했음.
    
    #34
    
    * Chore: 사이드바에 필요한 아이콘 추가
    
    앨범 추가, 그룹 추가, 아래 화살표, 메뉴, 세팅 아이콘 public/icons에 추가
    
    #34
    
    * Chore: 프로젝트에 사용되는 색상 객체로 관리하도록 생성
    
    Figma 기반 색상들 객체로 정리
    
    #34
    
    * Feat: 사이드바 토글 기능 구현
    
    토글 버튼을 헤더에 위치시킬 것이므로 일단 Main에서 토글 여부를 state로
    관리하는 것으로 설정함. 꺼지면 그룹 탭을 보이지 않도록 처리함.
    
    #34
    
    * Feat: @ alias를 통해 절대 경로로 파일 import하는 기능 추가
    
    craco 패키지 설치하고, 관련 설정들을 해줌.
    
    #34
    
    * Improve: img 태그의 alt 수정
    
    스크린 리더와 대체 텍스트로 보기에 더 명확히 식별 가능하다는 리뷰를 받고
    수정함.
    
    #35
    
    * Improve: type을 interface로 수정
    
    interface로 통일하기 위함.
    
    #35
    
    * Improve: path alias 미적용된 것 수정
    
    이전 디렉토리로 이동하는 것은 앞으로 path alias로 작성할 예정.
    
    #35

---
+++ frontend/src/components/Sidebar/FirstDepth/index.tsx
@@ -0,0 +1,56 @@
+import { Dispatch, SetStateAction } from "react";
+import styled from "styled-components";
+import Group from "./Group";
+import AddGroupButton from "./AddGroupButton";
+import Color from "@components/Styles/Color";
+
+interface SidebarProps {
+  isToggle: boolean;
+  setIsToggle: Dispatch<SetStateAction<boolean>>;
+}
+
+const groupDummy = [
+  {
+    groupName: "그룹 A",
+    img: "",
+  },
+  {
+    groupName: "그룹 B",
+    img: "",
+  },
+  {
+    groupName: "그룹 C",
+    img: "",
+  },
+];
+
+const FirstDepth = ({ isToggle, setIsToggle }: SidebarProps) => {
+  const onClickMenu = () => {
+    setIsToggle(prev => !prev);
+  };
+
+  return (
+    <>
+      <FirstDepthWrapper>
+        {groupDummy.map(group => (
+          <Group groupName={group.groupName} />
+        ))}
+        <AddGroupButton />
+        <div>
+          <img src="/icons/menu.svg" onClick={onClickMenu} alt="menu icon.svg" />
+        </div>
+      </FirstDepthWrapper>
+    </>
+  );
+};
+
+const FirstDepthWrapper = styled.div`
+  width: 5vw;
+  height: 100vh;
+  background-color: ${Color["theme1-primary"]};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default FirstDepth;
