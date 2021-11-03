9d8a982c51cdf08615c01631278978660c9fd568
J005_공필상 <34854154+gofeel8@users.noreply.github.com>
2021-11-03 13:41:54 +0900

    이미지 업로드 모달 구현 (#46)
    * Feat: PostCreateModal 안의 내용 컴포넌트로 분리
    
    PostCreateModal 안에서 필요한 모달이 2개이므로 한번에 구현하지 않고 컴포넌트를 나눠 개발하기 위함
    
    #5
    
    * Feat: Modal border readius 추가
    
    둥글둥글한 모습의 모달을 위한 속성 추가
    
    #5
    
    * Feat: UploadImageModal 헤더 타이틀 변경 및 닫기 버튼 추가
    
    사진 입력받는 모달창 수정
    
    #5
    
    * Feat: 이미지 업로드 모달 그리드 레이아웃 완성
    
    그리드 레이아웃 구현
    
    #5
    
    * Feat: 이미지 업로드 버튼 구현
    
    사용자로부터 이미지를 입력받기 위한 버튼 구현
    
    [#5 , #6]
    
    * Feat: 사진 업로드 구현
    
    input 태그를 통해 최대 5장 사진(상태)을  받고 프리뷰를 보여준다.
    
    [#7 , #5]
    
    * Feat: 사진 최소 1장 업로드 후 다음으로 넘어가는 버튼 활성화
    
    사진을 최소 한장 이상 업로드 하게 되면 X 버튼 자리에 > 버튼 활성
    
    [#6. #5]
    
    * Feat: 사진 삭제 버튼 구현
    
    사진 우측 위 빨간 x 버튼을 눌러 사진을 삭제 한다
    
    #5
    
    * Refactor: 조건문 등호 2개 => 3개 수정
    
    * Fix: !=== 잘 못 쓴 부분 !== 로 수정

--- frontend/src/components/Modal/PostCreateModal/index.tsx
+++ frontend/src/components/Modal/PostCreateModal/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Modal from "../";
+import UploadImageModal from "./UploadImageModal";
 
 interface PostCreateModalProps {
   closeFn: () => void;
@@ -10,24 +11,9 @@ interface PostCreateModalProps {
 const PostCreateModal = ({ closeFn, open = false }: PostCreateModalProps) => {
   return (
     <Modal open={open} closeFn={closeFn}>
-      <ModalContainer
-        onClick={event => {
-          event.nativeEvent.stopImmediatePropagation();
-        }}
-      >
-        <div>모달창</div>
-        <button type="button" onClick={closeFn}>
-          x
-        </button>
-      </ModalContainer>
+      <UploadImageModal closeFn={closeFn} />
     </Modal>
   );
 };
 
 export default PostCreateModal;
-
-const ModalContainer = styled.div`
-  background-color: #ffffff;
-  height: 30vw;
-  width: 50vw;
-`;
