3f8946c71f0a629f3d8a80514e67067e65ff52af
soosungp33 <soosungp33@gmail.com>
2021-11-03 18:52:25 +0900

    Refactor: DB 구조 변경

---
+++ backend/src/configs/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import "dotenv/config";
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: "mysql",
+  host: process.env.JUSTUS_MYSQL_HOST,
+  port: +process.env.JUSTUS_MYSQL_PORT,
+  username: process.env.JUSTUS_MYSQL_USER,
+  password: process.env.JUSTUS_MYSQL_PASSWORD,
+  database: process.env.JUSTUS_MYSQL_DATABASE, //process.env.MYSQL_DATABASE,
+  entities: [__dirname + process.env.JUSTUS_ENTITIY_PATH], //[process.env.ENTITIY_PATH],
+  synchronize: true, // false로 해두는 게 안전하다.
+};
+console.log(typeORMConfig);
