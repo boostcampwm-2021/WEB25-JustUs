f71aac845b238b460b3be3c340cb7ba7fb442692
Jo-Myounghee <jomyounghee32@gmail.com>
2021-11-04 17:39:15 +0900

    Feat: 검색 시 검색 결과를 모달 리스트로 확인할 수 있다.
    검색 입력창에 글자 입력 후 enter를 누르면 모달창이 생성되고,
    리스트 아이템을 클릭하면 해당 글자가 input에 나타나면서 모달창이 닫히게 구현
    
    #29

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import { Dispatch, SetStateAction } from "react";
 import styled from "styled-components";
-import Profile from "@components/Header/Profile";
-import { flexRowCenterAlign } from "@styles/StyledComponents";
 import COLOR from "@styles/Color";
 import { useSelector } from "react-redux";
 import { RootState } from "@src/reducer";
+
+import Profile from "@components/Header/Profile";
+import Search from "@components/Header/Profile/Search";
 
 interface SidebarProps {
   isToggle: boolean;
@@ -22,10 +23,7 @@ const Header = ({ isToggle, setIsToggle }: SidebarProps) => {
     <>
       <HeaderContainer groups={groups}>
         <img src="/icons/menu.svg" className="pointer" onClick={onClickMenu} alt="menu" />
-        <SearchContainer>
-          <img src="/icons/search.svg" height="90%" alt="search" />
-          <Search type="text" placeholder="해시태그를 입력하세요." />
-        </SearchContainer>
+        <Search />
         <Profile />
       </HeaderContainer>
     </>
@@ -45,26 +43,6 @@ const HeaderContainer = styled.div<{ groups: any }>`
     &:hover {
       cursor: ${({ groups }) => (groups.length ? "pointer" : "not-allowed")};
     }
-  }
-`;
-
-const SearchContainer = styled.div`
-  ${flexRowCenterAlign}
-  height: 3vh;
-  width: 25vw;
-  background-color: ${COLOR.WHITE};
-  border-radius: 5px;
-  padding: 0.5vh 0;
-  & > img {
-    padding: 0 0.5vw;
-  }
-`;
-
-const Search = styled.input`
-  height: 90%;
-  border: none;
-  &:focus-visible {
-    outline: none;
   }
 `;
 
