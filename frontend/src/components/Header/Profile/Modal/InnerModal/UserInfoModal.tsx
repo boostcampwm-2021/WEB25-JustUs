afbcca856f5707919692ed59ca9e93024d879933
Jo-Myounghee <jomyounghee32@gmail.com>
2021-11-04 17:43:01 +0900

    Refactor: 코드 내 사용하지 않는 변수 제거 및 css 변경

--- frontend/src/components/Header/Profile/Modal/InnerModal/UserInfoModal.tsx
+++ frontend/src/components/Header/Profile/Modal/InnerModal/UserInfoModal.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import Modal from "@components/Modal";
 import { useDispatch } from "react-redux";
