5982820d6c47a81073bd3c5d3653e249cd2d40fc
soosungp33 <soosungp33@gmail.com>
2021-11-06 23:01:46 +0900

    Refactor: 유저 생성 or 검사 시 Dto 생성
    userEmail, userNickName은 필수
    profileImage는 옵션

--- backend/src/auth/strategy/naver.strategy.ts
+++ backend/src/auth/strategy/naver.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-naver-v2";
+import { RegisterUserDto } from "src/dto/register-user.dto";
 import { AuthService } from "../service/auth.service";
 
 @Injectable()
@@ -15,8 +16,12 @@ export class NaverStrategy extends PassportStrategy(Strategy) {
 
   async validate(accessToken: string, refreshToken: string, profile: any): Promise<any> {
     const { email, nickname, profileImage } = profile;
+    const registerUserDto: RegisterUserDto = new RegisterUserDto();
+    registerUserDto.userEmail = email;
+    registerUserDto.userNickname = nickname;
+    registerUserDto.profileImage = profileImage;
 
-    const user = await this.authService.validateUser(email, nickname, profileImage);
+    const user = await this.authService.validateUser(registerUserDto);
 
     return user;
   }
