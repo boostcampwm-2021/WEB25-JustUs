fb20f182960e8e83ba40813131f396b498a5e3c3
soosungp33 <soosungp33@gmail.com>
2021-11-06 22:13:31 +0900

    Feat: naver login 구현
    - Guard <--> Strategy 전략을 이용해 구현
    - 로그인시 NaverOauthGuard를 거치는데 passport-naver를 설정해주어 간단하게 naver login을 연동한다.

--- backend/src/oauth/controller/oauth.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Controller, Get, Req, Res, UseGuards } from "@nestjs/common";
-import { Request, Response } from "express";
-
-@Controller("/api/oauth")
-export class OauthController {}
--- backend/src/oauth/oauth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { OauthController } from "./controller/oauth.controller";
-import { OauthService } from "./service/oauth.service";
-
-@Module({
-  controllers: [OauthController],
-  providers: [OauthService],
-})
-export class OauthModule {}
