9b879fa2a6cd0e7a450685eca0e9807f9548c8ce
soosungp33 <soosungp33@gmail.com>
2021-11-09 00:08:35 +0900

    Refactor: update UserInfoResponseDto name

--- backend/src/dto/user/userInfoResponse.dto.ts
+++ backend/src/dto/user/userInfoResponse.dto.ts
@@ -1,6 +1,6 @@
 import { IsString, IsNotEmpty } from "class-validator";
 
-export class FindUserResponseDto {
+export class UserInfoResponseDto {
   @IsString()
   @IsNotEmpty()
   userNickname: string;
