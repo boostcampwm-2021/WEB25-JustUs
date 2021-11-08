95b9565f1eaf11675e8d5bb04b1134ce84957eae
Jo-Myounghee <jomyounghee32@gmail.com>
2021-11-08 18:57:04 +0900

    Feat: 더미데이터를 이용해 지도에 포도알 여러 개 추가
    위도, 경도 더미데이터 값이 한국 위치 값을 넘어가면 렌더링이 안되어 한국 위치 내 위치 값으로 값 변경
    
    #68

--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -30,8 +30,16 @@ const setMap = (INIT_X: number, INIT_Y: number, ZOOM_SIZE: number) => {
     center: pos,
     zoom: ZOOM_SIZE,
   });
+  const markerItems = [
+    { id: 0, name: "삼겹살", position: [37.3595704, 127.105399] },
+    { id: 1, name: "맥도날드", position: [37.3618025, 127.1153248] },
+    { id: 2, name: "미삼집", position: [37.3561936, 127.0983706] },
+  ];
 
-  const marker = new naver.maps.Marker(Marker(map, pos));
+  markerItems.forEach((marker) => {
+    const pos1 = new naver.maps.LatLng(marker.position[0], marker.position[1]);
+    new naver.maps.Marker(Marker(map, pos1, marker.id));
+  });
 };
 
 const setError = () => {
