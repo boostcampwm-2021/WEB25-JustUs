1a32db201a8f0353b314963c1e5cfb78bb559a51
soosungp33 <soosungp33@gmail.com>
2021-11-09 00:08:35 +0900

    Refactor: update UserInfoResponseDto name

--- backend/src/user/controller/user.controller.ts
+++ backend/src/user/controller/user.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { Controller, Get, Param, Put, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "src/auth/guard/jwt-auth-guard";
 import { UserService } from "../service/user.service";
-import { FindUserResponseDto } from "src/dto/user/userInfoResponse.dto";
+import { UserInfoResponseDto } from "src/dto/user/userInfoResponse.dto";
 
 @UseGuards(JwtAuthGuard)
 @Controller("api/users")
@@ -9,7 +9,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get("/:userId")
-  getUserInfo(@Param("userId") userId: number): Promise<FindUserResponseDto> {
+  getUserInfo(@Param("userId") userId: number): Promise<UserInfoResponseDto> {
     return this.userService.findUserInfo(userId);
   }
 }
