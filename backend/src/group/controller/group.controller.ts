56e351e6ffcf31825a9174044813dd2a3b45a48e
soosungp33 <soosungp33@gmail.com>
2021-11-09 22:45:28 +0900

    Feat: 그룹 생성 API 구현
    Body
    - 필수 : userId, groupName
    - 옵션 : groupImage
    
    Response : 생성된 groupId

--- backend/src/group/controller/group.controller.ts
+++ backend/src/group/controller/group.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/guard/jwt-auth-guard";
+import { GroupService } from "../service/group.service";
+import { CreateGroupRequestDto } from "src/dto/group/createGroupRequest.dto";
 
-@Controller("group")
-export class GroupController {}
+@UseGuards(JwtAuthGuard)
+@Controller("api/groups")
+export class GroupController {
+  constructor(private readonly groupService: GroupService) {}
+
+  @Post()
+  @HttpCode(200)
+  CreateGroup(@Body() createGroupRequestDto: CreateGroupRequestDto): Promise<number> {
+    return this.groupService.createGroup(createGroupRequestDto);
+  }
+}
