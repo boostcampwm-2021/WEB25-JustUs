d09b78b1eb4b7634b41245257cd7ae8006dca15d
soosungp33 <soosungp33@gmail.com>
2021-11-09 22:43:51 +0900

    Feat: User-Group간 ManyToMany 설정

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from "typeorm";
 import { TimeStampEntity } from "src/myBaseEntity/TimestampEntity";
+import { Group } from "src/group/group.entity";
 
 @Entity({ name: "users" })
 export class User extends TimeStampEntity {
@@ -14,4 +15,8 @@ export class User extends TimeStampEntity {
 
   @Column()
   userEmail: string;
+
+  @ManyToMany(() => Group)
+  @JoinTable({ name: "users_groups_TB" })
+  groups: Group[];
 }
