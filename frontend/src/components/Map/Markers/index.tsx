675f1aa7adf251c2db491e233cdf5a7374000c6d
myoung <48939876+Jo-Myounghee@users.noreply.github.com>
2021-11-09 10:43:13 +0900

    지도에 마커 추가 (#75)
    * Design: map이 선택되었을 때 outline이 생기는 현상 수정
    
    기존에 네이버 맵 부분을 마우스로 클릭하면 주변에 outline이 생기는 현상이 존재했는데, 해당 부분 outline 삭제
    
    #68
    
    * Feat: 지도 위 포도 마커 추가
    
    지도 위 임의의 위치에 포도 마커 추가
    
    #68
    
    * Feat: 마커에 갯수 표시
    
    포도알 우측 상단에 숫자 표시하는 공간 생성을 위해 HtmlIcon 객체 사용
    
    #68
    
    * Feat: 더미데이터를 이용해 지도에 포도알 여러 개 추가
    
    위도, 경도 더미데이터 값이 한국 위치 값을 넘어가면 렌더링이 안되어 한국 위치 내 위치 값으로 값 변경
    
    #68

---
+++ frontend/src/components/Map/Markers/index.tsx
@@ -0,0 +1,31 @@
+const Marker = (map: naver.maps.Map, pos: naver.maps.LatLng, count: number = 3) => {
+  const markerCountStyle = `
+    position: absolute;
+    background-color: black;
+    border-radius: 1rem;
+    z-index: 10;
+    top: 0;
+    right: 0;
+    width: 1rem;
+    height: 1rem;
+    text-align: center;
+    color: white;
+  `;
+
+  const markerIcon = `
+    <img src="/icons/podo.png" alt="marker">
+    <div class="markerCount" style="${markerCountStyle}">${count}</div>
+  `;
+
+  return {
+    position: pos.destinationPoint(90, 15),
+    map: map,
+    icon: {
+      content: markerIcon,
+    },
+    size: new naver.maps.Size(22, 22),
+    origin: new naver.maps.Point(0, 0),
+  };
+};
+
+export default Marker;
--- frontend/src/components/Map/index.tsx
+++ frontend/src/components/Map/index.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import COLOR from "@styles/Color";
 import { flexRowCenterAlign } from "@styles/StyledComponents";
 import { useDispatch } from "react-redux";
+import Marker from "@components/Map/Markers";
 
 const load = (url: string, cb: Function, err: Function) => {
   const element = document.createElement("script");
@@ -11,7 +12,10 @@ const load = (url: string, cb: Function, err: Function) => {
 
   element.async = true;
   element.onload = () => {
-    cb();
+    const INIT_X = 37.511337;
+    const INIT_Y = 127.012084;
+    const ZOOM_SIZE = 13;
+    cb(INIT_X, INIT_Y, ZOOM_SIZE);
   };
   element.onerror = () => {
     err();
@@ -19,6 +23,29 @@ const load = (url: string, cb: Function, err: Function) => {
   element[attr] = url;
   document[parent].appendChild(element);
 };
+
+const setMap = (INIT_X: number, INIT_Y: number, ZOOM_SIZE: number) => {
+  const pos = new naver.maps.LatLng(INIT_X, INIT_Y);
+  const map = new naver.maps.Map("map", {
+    center: pos,
+    zoom: ZOOM_SIZE,
+  });
+  const markerItems = [
+    { id: 0, name: "삼겹살", position: [37.3595704, 127.105399] },
+    { id: 1, name: "맥도날드", position: [37.3618025, 127.1153248] },
+    { id: 2, name: "미삼집", position: [37.3561936, 127.0983706] },
+  ];
+
+  markerItems.forEach((marker) => {
+    const pos1 = new naver.maps.LatLng(marker.position[0], marker.position[1]);
+    new naver.maps.Marker(Marker(map, pos1, marker.id));
+  });
+};
+
+const setError = () => {
+  console.error("잘못되었습니다.");
+};
+
 const Map = () => {
   const dispatch = useDispatch();
 
@@ -26,18 +53,7 @@ const Map = () => {
     const initMap = () => {
       const clientId: string = process.env.REACT_APP_NCP_CLOUD_ID as string;
       const url = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${clientId}`;
-      load(
-        url,
-        () => {
-          const map = new naver.maps.Map("map", {
-            center: new naver.maps.LatLng(37.511337, 127.012084),
-            zoom: 13,
-          });
-        },
-        () => {
-          console.error("잘못되었습니다.");
-        },
-      );
+      load(url, setMap, setError);
     };
     initMap();
   }, []);
@@ -58,6 +74,9 @@ const Maps = styled.div`
   position: absolute;
   width: 100%;
   height: 95vh;
+  &:focus-visible {
+    outline: none;
+  }
 `;
 
 const FloatActionBtn = styled.button`
