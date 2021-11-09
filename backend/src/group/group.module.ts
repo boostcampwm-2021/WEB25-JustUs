28615c8d731ef1a739bf9943d4dd3418071e0e36
soosungp33 <soosungp33@gmail.com>
2021-11-09 22:45:28 +0900

    Feat: 그룹 생성 API 구현
    Body
    - 필수 : userId, groupName
    - 옵션 : groupImage
    
    Response : 생성된 groupId

--- backend/src/group/group.module.ts
+++ backend/src/group/group.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserRepository } from "src/user/user.repository";
 import { GroupController } from "./controller/group.controller";
 import { GroupRepository } from "./group.repository";
 import { GroupService } from "./service/group.service";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([GroupRepository]),
+    TypeOrmModule.forFeature([GroupRepository, UserRepository]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
     }),
