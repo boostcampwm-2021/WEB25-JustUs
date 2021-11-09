cf51327ee9d15d1eb65adcd24bb024648dae84cd
soosungp33 <soosungp33@gmail.com>
2021-11-09 15:14:21 +0900

    Feat: init Group 디렉토리

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { typeORMConfig } from "./configs/typeorm.config";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
+import { GroupModule } from "./group/group.module";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule, AuthModule, GroupModule],
 })
 export class AppModule {}
