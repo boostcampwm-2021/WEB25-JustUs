f4f2c310eec112abcd48fd2ebe346c7a3ab1eff0
soosungp33 <soosungp33@gmail.com>
2021-11-11 18:50:58 +0900

    Refactor: update userinfo name

--- backend/src/user/user.repository.ts
+++ backend/src/user/user.repository.ts
@@ -1,10 +1,10 @@
-import { UserInfoDto } from "src/dto/user/userInfo.dto";
+import { UserInfo } from "src/dto/user/userInfo.dto";
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-  async saveUser(registerUserDto: UserInfoDto): Promise<User> {
+  async saveUser(registerUserDto: UserInfo): Promise<User> {
     const user = this.create(registerUserDto);
 
     return await this.save(user);
