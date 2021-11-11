58b6dfc1eeceab5a9ad44e70f01074955fbe5726
myoung <48939876+Jo-Myounghee@users.noreply.github.com>
2021-11-12 00:42:30 +0900

    Revert "CI/CD - Dockerfile 작성 (#36)"
    This reverts commit ec38eec1352face5ef14bc50c3270d1589f5b8a3.

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserController } from "./controller/user.controller";
-import { UserService } from "./service/user.service";
-import { User } from "./user.entity";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
+import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
