207e09119d1280f4097cf86c04b96e2ac8ae828d
J005_공필상 <34854154+gofeel8@users.noreply.github.com>
2021-11-11 11:00:31 +0900

    해시태그 추천 구현 (#104)
    * Feat: 검색 결과 모달창 닫는 이벤트 추가
    
    모달 외 클릭시 & 키보드 추가 입력시 모달 창 닫음
    
    #29
    
    * Style : 검색 관련 css 수정
    
    검색바 & 결과 모달 css 수정
    
    #29
    
    * Feat: 해시태그 키워드 추천 구현
    
    검색 인풋창 키 이벤트를 받아 관련 키워드 해시태그를 추천해 준다.
    
    #101
    
    * Fix: Mac  드래그 기본 애니메이션 오류 해결
    
    preventDefault() 을 통해서 해결

--- frontend/src/components/Header/Profile/Search/SearchList/index.tsx
+++ frontend/src/components/Header/Profile/Search/SearchList/index.tsx
@@ -14,17 +14,17 @@ interface SearchListProps {
 
 const SearchList = ({ setSearchKeyword, setIsSearchListOpened }: SearchListProps) => {
   const searchListContents: SearchListContent[] = [
-    { id: 0, name: "미삼집" },
-    { id: 1, name: "맥도날드" },
-    { id: 2, name: "농성화로" },
-    { id: 3, name: "아웃백" },
-    { id: 4, name: "버거킹" },
-    { id: 5, name: "롯데리아" },
-    { id: 6, name: "kfc" },
-    { id: 7, name: "포도주" },
-    { id: 8, name: "솥뚜껑" },
-    { id: 9, name: "팔팔소곱창" },
-    { id: 10, name: "청해" },
+    { id: 0, name: "미삼집 관련 게시글" },
+    { id: 1, name: "맥도날드 관련 게시글" },
+    { id: 2, name: "농성화로 관련 게시글" },
+    { id: 3, name: "아웃백 관련 게시글" },
+    { id: 4, name: "버거킹 관련 게시글" },
+    { id: 5, name: "롯데리아 관련 게시글" },
+    { id: 6, name: "kfc 관련 게시글" },
+    { id: 7, name: "포도주 관련 게시글" },
+    { id: 8, name: "솥뚜껑 관련 게시글" },
+    { id: 9, name: "팔팔소곱창 관련 게시글" },
+    { id: 10, name: "청해 관련 게시글" },
   ];
 
   const handleClickSearchListItem = (e: HTMLElement) => {
@@ -49,7 +49,6 @@ const SearchList = ({ setSearchKeyword, setIsSearchListOpened }: SearchListProps
     </SearchListContainer>
   );
 };
-
 const SearchListContainer = styled.div`
   position: absolute;
   background-color: ${COLOR.WHITE};
@@ -58,16 +57,17 @@ const SearchListContainer = styled.div`
   z-index: 6;
   width: 25vw;
   font-size: 1rem;
+  box-sizing: border-box;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 0.6vw;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${COLOR.LIGHTGRAY2};
+    border-radius: 5px;
+  }
   & ul {
-    height: 20vh;
-    overflow-y: scroll;
-    &::-webkit-scrollbar {
-      width: 1vw;
-    }
-    &::-webkit-scrollbar-thumb {
-      background-color: ${COLOR.LIGHTGRAY2};
-      border-radius: 5px;
-    }
+    max-height: 20vh;
     margin: 1.5vw 0 1.5vw 1.5vw;
     & li {
       padding: 1vh 0 1vh 1vh;
@@ -82,8 +82,7 @@ const SearchListContainer = styled.div`
         padding-top: 0;
       }
       &:nth-last-child(1) {
-        border-bottom: none;
-        padding-bottom: 0;
+        padding-bottom: 1.2vh;
       }
     }
   }
