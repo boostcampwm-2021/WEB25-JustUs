d48802678879a03f0e3037209bc58c0e2aa41d8e
myoung <48939876+Jo-Myounghee@users.noreply.github.com>
2021-11-12 00:42:30 +0900

    Revert "Config: 프론트엔드 환경 설정 (#1)"
    This reverts commit 0306421ba5dc67fe812d5f3b0a4395eb1ff00fe4.

--- frontend/src/pages/Login/Index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Login() {
-  return;
-}
-
-export default Login;
--- frontend/src/pages/Main/Index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Main() {
-  return;
-}
-
-export default Main;
