481095ac51f4e8d5e4ddc749d02c15281b587f2a
myoung <48939876+Jo-Myounghee@users.noreply.github.com>
2021-11-15 14:52:02 +0900

    Feat: 마우스 오른쪽 버튼을 통해 게시글을 추가할 때, 장소명을 사용자로부터 받는다. (#129)
    우클릭 시 좌표 정보는 존재하나 장소명이 존재하지 않으므로 사용자로부터 입력받을 수 있게 구현.
    서로 다른 모달에서 장소명을 사용해야하므로 전역에서 장소 상태를 관리한다.
    
    #128

--- frontend/src/components/Modal/ModalManager/index.tsx
+++ frontend/src/components/Modal/ModalManager/index.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+import UploadAddressModal from "@components/Map/Modal/UploadAddressModal";
 import PostCreateModal from "../PostCreateModal";
 import PostShowModal from "../PostShowModal";
 import CreateGroupModal from "@components/Sidebar/FirstDepth/AddGroupButton/Modal/InnerModal/CreateGroupModal";
@@ -25,6 +26,7 @@ const ModalManager = ({ setIsToggle }: ModalManagerProps) => {
   return (
     <>
       {nowModal === "" && <CloseModal />}
+      {nowModal === "UploadAddressModal" && <UploadAddressModal />}
       {nowModal === "PostCreateModal" && <PostCreateModal />}
       {nowModal === "PostShowModal" && <PostShowModal />}
       {nowModal === "CreateGroupModal" && <CreateGroupModal />}
