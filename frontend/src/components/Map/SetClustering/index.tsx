e04044ddfca332a977ac40a66daed48ded0b80dd
gofeel8 <gofeel8@hanmail.net>
2021-11-16 02:21:48 +0900

    Feat: 클러스터링 마커 아래 포함된 마커의 개수 보여주기
    마커 이미지 옆에 P태그를 추가하여 안에 개수를 보여준다
    
    #139

--- frontend/src/components/Map/SetClustering/index.tsx
+++ frontend/src/components/Map/SetClustering/index.tsx
@@ -2,8 +2,8 @@ const Clustering = (map: naver.maps.Map, markers: naver.maps.Marker[]) => {
   const SIZE = 40;
   const POINT = 20;
   const MIN_CLUSTER_SIZE = 2;
-  const MAX_ZOOM_SIZE = 13;
-  const GRID_SIZE = 120;
+  const MAX_ZOOM_SIZE = 18;
+  const GRID_SIZE = 70;
 
   const podoThree = {
     content: `<img src="/icons/podo-three.png" alt="three grape">`,
@@ -25,6 +25,14 @@ const Clustering = (map: naver.maps.Map, markers: naver.maps.Marker[]) => {
     gridSize: GRID_SIZE,
     icons: [podoThree, podoMany],
     indexGenerator: [4, 6],
+    averageCenter: true,
+    stylingFunction: function (clusterMarker: any, count: number) {
+      const pTag = document.createElement("p");
+      pTag.innerHTML = count.toString();
+      pTag.style.cssText =
+        "display: inline-block;position:absolute;right:-5px;bottom:-5px;height:20px;line-height:20px;width:20px;background-color:purple;color:white;border-radius: 50%;text-align:center";
+      clusterMarker.getElement().appendChild(pTag);
+    },
   };
 };
 
