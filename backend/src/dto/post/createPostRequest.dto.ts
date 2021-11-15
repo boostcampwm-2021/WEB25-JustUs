5419421cd67dab57a463049d9f79a7b5375f2320
Younghan <soosungp33@gmail.com>
2021-11-16 01:39:56 +0900

    Feat : string으로 들어올 시 number로 변경 후 validator 검사

--- backend/src/dto/post/createPostRequest.dto.ts
+++ backend/src/dto/post/createPostRequest.dto.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsNumber } from "class-validator";
 import { PostInfo } from "./postInfo";
 import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
 
 export class CreatePostRequestDto extends PostInfo {
-  //@IsNumber()
+  @IsNumber()
   @IsNotEmpty()
+  @Transform(({ value }) => Number(value))
   @ApiProperty()
   albumId: number;
 }
