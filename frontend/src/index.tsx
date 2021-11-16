9cd838220731e3dbb359c1f4d9789c772146543f
Jo-Myounghee <jomyounghee32@gmail.com>
2021-11-17 03:21:53 +0900

    Feat: redux-logger 적용
    콘솔창에서 redux 상태를 확인하기 위해 redux-logger 적용
    
    #147

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import rootReducer from "@src/reducer";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
-const enhancer = compose(applyMiddleware(sagaMiddleware));
+const enhancer = composeWithDevTools(applyMiddleware(sagaMiddleware, logger));
 const store = createStore(rootReducer, enhancer);
 
 sagaMiddleware.run(rootSaga);
