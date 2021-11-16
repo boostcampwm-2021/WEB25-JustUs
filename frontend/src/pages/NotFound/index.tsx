7e3b71255e47af56ae2b12b2f64f1441c0133b8f
김무성 <ddb8036631@gmail.com>
2021-11-16 21:14:56 +0900

    Feat: 잘못된 URL 입력시 예외 페이지로 라우팅 구현
    관련 issue: #133

---
+++ frontend/src/pages/NotFound/index.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const NotFound = () => {
+  return (
+    <NotFoundWrapper>
+      <ImageWrapper>
+        <Guide>잘못된 페이지에요 ㅠㅠ</Guide>
+      </ImageWrapper>
+    </NotFoundWrapper>
+  );
+};
+
+const NotFoundWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  background: center;
+  background-image: url("/icons/crying-podo.png");
+  background-size: 40%;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 95vh;
+`;
+const Guide = styled.div`
+  font-size: 5rem;
+  margin-bottom: 1vh;
+`;
+
+export default NotFound;
