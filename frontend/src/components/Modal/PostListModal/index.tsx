77c735aedde20e5d7333777eb273cb571d99a242
J005_공필상 <34854154+gofeel8@users.noreply.github.com>
2021-11-17 14:00:52 +0900

    클러스터링 마커 리스트 모달 구현 (#152)
    * Feat: 드래그시 포스트 추가 작은 모달 닫히도록 로직 추가
    
    * Feat: 클러스터 마커 클릭시 지도 내 마커 리스트 띄우기
    
    클러스터링 마커를 클릭 시 지도의 infowindow 를 이용해 포함된 마커 리스트를 보여 줍니다.
    
    #66
    
    * Feat: 클러스터에 속한 개수 보여주는 태그 css 수정
    
    * Improve: px->rem 변경

--- frontend/src/components/Modal/PostListModal/index.tsx
+++ frontend/src/components/Modal/PostListModal/index.tsx
@@ -1,131 +1,62 @@
-import Modal from "@components/Modal";
-import styled, { keyframes } from "styled-components";
-import { flexRowCenterAlign } from "@styles/StyledComponents";
-import COLOR from "@styles/Color";
-import { useSelector } from "react-redux";
-import { RootState } from "@src/reducer";
-import { useDispatch } from "react-redux";
+import styled from "styled-components";
 
-const PostListModal = () => {
-  const dispatch = useDispatch();
-  const { clusteredMarker }: any = useSelector((state: RootState) => state.modal);
+interface Imarker {
+  postTitle: string | undefined;
+  postId: number;
+}
 
-  const closeModal = () => {
-    dispatch({ type: "CLOSE_MODAL" });
-  };
+interface Theme {
+  [Key: string]: string;
+}
 
+interface IPostList {
+  clusteredMarker: Imarker[];
+  handleClickMarker: (postId: number) => void;
+  nowTheme: Theme;
+}
+
+const PostListModal = ({ clusteredMarker, handleClickMarker, nowTheme }: IPostList) => {
   return (
-    <Modal>
-      <ModalContainer
-        onClick={(event) => {
-          event.nativeEvent.stopImmediatePropagation();
-        }}
-      >
-        <Header>
-          <CloseBtn>
-            <button type="button" onClick={closeModal}>
-              <img src="/icons/clear.svg" alt="clear icon" />
-            </button>
-          </CloseBtn>
-          <Title>클러스터 마커 리스트 </Title>
-          <Content>
-            <ul>
-              {clusteredMarker.map((marker: any) => (
-                <li key={marker.postId}>
-                  Title: {marker.postTitle}-id:{marker.postId}
-                </li>
-              ))}
-            </ul>
-          </Content>
-        </Header>
-      </ModalContainer>
-    </Modal>
+    <ItemContainer color={nowTheme.SECONDARY}>
+      {clusteredMarker.map((marker: Imarker) => (
+        <MarkerItem key={marker.postId} color={nowTheme.SECONDARY} onClick={() => handleClickMarker(marker.postId)}>
+          {marker.postTitle}
+        </MarkerItem>
+      ))}
+    </ItemContainer>
   );
 };
-
-const modalSlideUp = keyframes`
-  0% {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  30% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-const ModalContainer = styled.div`
-  background-color: ${COLOR.WHITE};
-  min-height: 35vh;
-  min-width: 30vw;
-  border-radius: 2rem;
-  display: flex;
-  flex-direction: column;
-  animation-name: ${modalSlideUp};
-  animation-duration: 1s;
-`;
-const Header = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-const CloseBtn = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  margin-top: 30px;
-  margin-right: 30px;
-
-  & > button {
-    background-color: ${COLOR.WHITE};
-    border: none;
-  }
-`;
-const Title = styled.div`
-  font-size: 40px;
-`;
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 2rem;
-`;
-const DeleteAlbumGuideWrapper = styled.div`
-  width: 100%;
-  margin-top: 30px;
-  font-size: 1.5rem;
-  text-align: center;
-`;
-
-const BtnWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 40% 20% 40%;
-`;
-const CancelBtnWrapper = styled.div`
-  ${flexRowCenterAlign}
-  width: 8rem;
-  height: 39px;
-  border-radius: 10px;
-  border: 1px solid ${COLOR.BLUE};
-  color: ${COLOR.BLACK};
-  background-color: ${COLOR.WHITE};
-  margin-top: 50px;
-  font-size: 30px;
-  grid-column-start: 1;
-  grid-column-end: 2;
-  cursor: pointer;
-`;
-const DeleteBtnWrapper = styled.div`
-  ${flexRowCenterAlign}
-  width: 8rem;
-  height: 39px;
-  border-radius: 10px;
-  color: ${COLOR.WHITE};
-  background-color: ${COLOR.RED};
-  margin-top: 50px;
-  font-size: 30px;
-  grid-column-start: 3;
-  grid-column-end: 4;
-  cursor: pointer;
-`;
 export default PostListModal;
+
+const ItemContainer = styled.ul`
+  padding: 0.3rem;
+  width: 20rem;
+  height: 10rem;
+  overflow-y: scroll;
+  box-sizing: content-box;
+
+  &::-webkit-scrollbar {
+    width: 1rem;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.color};
+    border-radius: 1rem;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+`;
+
+const MarkerItem = styled.li`
+  height: 2rem;
+  padding: 0.4rem;
+  cursor: pointer;
+  line-height: 2rem;
+  font-size: 1.4rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  &:hover {
+    background-color: ${(props) => props.color};
+  }
+`;
