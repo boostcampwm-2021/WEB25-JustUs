06fa284a58f49e27d2115d334470676cfa3cd7ad
김무성 <ddb8036631@gmail.com>
2021-11-17 21:07:49 +0900

    Feat: 유저 정보 수정 API 로직 작성
    관련 issue: #134

--- frontend/src/sagas/user.ts
+++ frontend/src/sagas/user.ts
@@ -1,5 +1,12 @@
-import { all, fork, put, call, takeEvery } from "redux-saga/effects";
-import { USER_INFO_REQUEST, USER_INFO_SUCCEED, USER_INFO_FAILED } from "@src/reducer/UserReducer";
+import { all, fork, put, call, takeEvery, select } from "redux-saga/effects";
+import {
+  USER_INFO_REQUEST,
+  USER_INFO_SUCCEED,
+  USER_INFO_FAILED,
+  USER_INFO_UPDATE,
+  SET_UPDATED_USER_INFO,
+  SET_UPDATE_FAIL,
+} from "@src/reducer/UserReducer";
 import axios from "axios";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
@@ -13,9 +20,27 @@ interface ResponseGenerator {
   statusText?: string;
   json: Function;
 }
-
+interface IUser {
+  userNickName: string;
+  userProfile: string;
+  userInfoLoading: boolean;
+  userInfoSucceed: boolean;
+  userInfoError: boolean;
+  updateUserNickName: string;
+  updateUserProfile: string;
+}
 function getUserInfoApi() {
   return axios.get(`${SERVER_URL}/api/user`, { withCredentials: true });
+}
+
+async function updateUserInfoApi(user: IUser) {
+  const result = await axios.put(
+    `${SERVER_URL}/api/user`,
+    { userNickname: user.updateUserNickName, profileImage: "temp" },
+    { withCredentials: true },
+  );
+
+  return result;
 }
 
 function* getUserInfo() {
@@ -27,10 +52,28 @@ function* getUserInfo() {
   }
 }
 
+function* updateUserInfo() {
+  const user: IUser = yield select((state) => state.user);
+
+  try {
+    yield call(updateUserInfoApi, user);
+    yield put({
+      type: SET_UPDATED_USER_INFO,
+      payload: { userNickName: user.updateUserNickName, userProfile: user.updateUserProfile },
+    });
+  } catch (err: any) {
+    yield put({ type: SET_UPDATE_FAIL });
+  }
+}
+
 function* watchUserInfo() {
   yield takeEvery(USER_INFO_REQUEST, getUserInfo);
 }
 
+function* watchUpdateUserInfo() {
+  yield takeEvery(USER_INFO_UPDATE, updateUserInfo);
+}
+
 export default function* userSaga() {
-  yield all([fork(watchUserInfo)]);
+  yield all([fork(watchUserInfo), fork(watchUpdateUserInfo)]);
 }
