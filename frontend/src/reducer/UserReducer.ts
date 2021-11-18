7569485a1b40c9cb87ba9e540e99263e7e246a6b
ddb8036631 <50266862+ddb8036631@users.noreply.github.com>
2021-11-18 11:47:35 +0900

    유저 API 로직 연결(유저 정보 수정) (#163)
    * Fix: 화면 너비별 px 다르게 설정, 게시글 조회 모달 높이 수정
    
    * Feat: 유저 정보 수정 API 로직 작성
    
    관련 issue: #134

--- frontend/src/reducer/UserReducer.ts
+++ frontend/src/reducer/UserReducer.ts
@@ -4,16 +4,28 @@ export const initState = {
   userInfoLoading: null,
   userInfoSucceed: null,
   userInfoError: null,
+  updateUserNickName: null,
+  updateUserProfile: null,
+  updateSucceed: null,
 };
 
 // action
 export const USER_INFO_REQUEST = "USER_INFO_REQUEST";
 export const USER_INFO_SUCCEED = "USER_INFO_SUCCEED";
 export const USER_INFO_FAILED = "USER_INFO_FAILED";
+export const USER_INFO_UPDATE = "USER_INFO_UPDATE";
+export const SET_UPDATED_USER_INFO = "SET_UPDATED_USER_INFO";
+export const SET_UPDATE_FAIL = "SET_UPDATE_FAIL";
+export const SET_UPDATED_INIT = "SET_UPDATED_INIT";
 
 //action creator
 export const userInfoRequestAction = () => ({
   type: USER_INFO_REQUEST,
+});
+
+export const userInfoUpdateAction = (payload: any) => ({
+  type: USER_INFO_UPDATE,
+  payload,
 });
 
 // reducer
@@ -41,6 +53,33 @@ export const userReducer = (state = initState, action: any) => {
         userInfoLoading: false,
         userInfoSucceed: false,
         userInfoError: action.error,
+      };
+    case USER_INFO_UPDATE:
+      return {
+        ...state,
+        updateUserNickName: action.payload.updateUserNickName,
+        updateUserProfile: action.payload.updateUserProfile,
+      };
+    case SET_UPDATED_USER_INFO:
+      return {
+        ...state,
+        updateUserNickName: null,
+        updateUserProfile: null,
+        userNickName: action.payload.userNickName,
+        userProfile: action.payload.userProfile,
+        updateSucceed: true,
+      };
+    case SET_UPDATE_FAIL:
+      return {
+        ...state,
+        updateUserNickName: null,
+        updateUserProfile: null,
+        updateSucceed: false,
+      };
+    case SET_UPDATED_INIT:
+      return {
+        ...state,
+        updateSucceed: null,
       };
     default:
       return state;
