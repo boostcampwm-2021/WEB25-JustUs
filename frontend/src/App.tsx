83d166d63135484a18cf46bb540e3ebbf6ad5753
J005_공필상 <34854154+gofeel8@users.noreply.github.com>
2021-11-23 11:01:00 +0900

    토큰 유효성 검증 후 리다이렉트 (#210)
    * Feat: 메인페이지 라우터 로직 구현
    
    로그인이 안된 유저 로그인 페이지로 이동 & 로그아웃 유저 로그인 페이지로 이동
    
    #195
    
    * Feat: 로그인페이지 라우터 로직 구현
    
    로그인한 유저가 로그인페이지 진입 시 메인페이지로 이동
    
    #195
    
    * Refact: userInfoRequest 로직 위치 변경
    
    중복 호출을 막기 위해 App.tsx에서 호출
    
    #195

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 import Router from "./Router";
 import GlobalStyle from "@styles/StyledComponents/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { useSelector } from "react-redux";
 import { RootState } from "@src/reducer";
+import { useDispatch } from "react-redux";
+import { userInfoRequestAction } from "@src/reducer/UserReducer";
 
 function App() {
   const { nowTheme } = useSelector((state: RootState) => state.theme);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({ type: "USER_INFO_INIT" });
+    dispatch(userInfoRequestAction());
+  }, []);
   return (
     <>
       <ThemeProvider theme={nowTheme}>
