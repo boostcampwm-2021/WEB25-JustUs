80bc9aef99683bb522c920ef52cd52dff038a5c2
김무성 <ddb8036631@gmail.com>
2021-11-23 23:11:17 +0900

    Fix: 로그아웃 후 ToastManager에 의해 로그인 버튼 안눌리는 현상 수정

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -62,6 +62,8 @@ const BackgroundImg = styled.div<{ imgUrl: string }>`
   background-image: url(${(props) => props.imgUrl});
   background-size: cover;
   height: 100vh;
+  z-index: 1;
+  position: relative;
 `;
 
 const BackgroundWrap = styled.div`
