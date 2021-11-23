445a5fc728534e659229647b5a14015fcf7100dd
Younghan <soosungp33@gmail.com>
2021-11-23 16:49:03 +0900

    Feat : Http 에러 핸들링

---
+++ backend/src/filter/httpException.filter.ts
@@ -0,0 +1,19 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from "@nestjs/common";
+import { Response } from "express";
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: exception.message,
+    });
+  }
+}
