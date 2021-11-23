15fc866af6323722d8e5bd386b3302ebe258238b
Younghan <soosungp33@gmail.com>
2021-11-24 00:41:55 +0900

    Refactor : make custom controller decorator

---
+++ backend/src/custom/decorator/controller.decorator.ts
@@ -0,0 +1,7 @@
+import { applyDecorators, Controller, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/auth/guard/jwt-auth-guard";
+
+export function CustomController(controller: string, apiTag: string) {
+  return applyDecorators(ApiTags(apiTag), ApiBearerAuth(), UseGuards(JwtAuthGuard), Controller(controller));
+}
