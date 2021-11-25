da5b689a08903a92da9b02643c4d7630de925505
김무성 <ddb8036631@gmail.com>
2021-11-25 23:06:45 +0900

    Feat: 테스트 로그인 구현

--- backend/src/auth/controller/auth.controller.ts
+++ backend/src/auth/controller/auth.controller.ts
@@ -5,12 +5,13 @@ import { NaverOauthGuard } from "../guard/naver-auth.guard";
 import { JwtAuthGuard } from "../guard/jwt-auth-guard";
 import { CustomRequest } from "src/custom/myRequest/customRequest";
 import { UserService } from "src/user/service/user.service";
+import { AuthService } from "../service/auth.service";
 
 @ApiTags("auth API")
 @ApiBearerAuth()
 @Controller("/auth")
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService, private readonly authService: AuthService) {}
 
   @Get("/login")
   @UseGuards(NaverOauthGuard)
@@ -44,5 +45,28 @@ export class AuthController {
     res.clearCookie("accessToken");
     res.clearCookie("refreshToken");
     res.end();
+  }
+
+  @Get("/demo/login")
+  async DemoLogin(@Res() res: Response) {
+    const userId = 9;
+    const userEmail = "test@test.com";
+
+    const accessTokenExpireTime = "1h";
+    const refreshTokenExpireTime = "7d";
+    const accessToken = this.authService.createToken(userId, userEmail, accessTokenExpireTime);
+    const refreshToken = this.authService.createToken(userId, userEmail, refreshTokenExpireTime);
+
+    await this.userService.updateToken(userId, refreshToken);
+
+    const cookieAccessTokenTokenExpireTime = 1000 * 60 * 60;
+    const cookieRefreshTokenExpireTime = cookieAccessTokenTokenExpireTime * 24 * 7;
+
+    res.cookie("accessToken", accessToken, { maxAge: cookieAccessTokenTokenExpireTime });
+    res.cookie("refreshToken", refreshToken, { maxAge: cookieRefreshTokenExpireTime });
+
+    const redirectUrl = process.env.NODE_ENV === "dev" ? process.env.DEV_REDIRECT_URL : process.env.PROD_REDIRECT_URL;
+
+    res.redirect(redirectUrl);
   }
 }
