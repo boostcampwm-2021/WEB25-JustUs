203af20310df5905ce534f3ddd76d6a1dfc75e24
Younghan <soosungp33@gmail.com>
2021-11-29 01:11:18 +0900

    Test : 테스트코드 뼈대 작성

--- backend/src/user/service/user.service.spec.ts
+++ backend/src/user/service/user.service.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { UserRepository } from "../user.repository";
 import { UserService } from "./user.service";
 
+const mockUserRepository = () => ({
+  save: jest.fn(),
+  findOne: jest.fn(),
+  update: jest.fn(),
+  getGroupsQuery: jest.fn(),
+});
+
+type MockUserRepository = Partial<Record<keyof UserRepository, jest.Mock>>;
+
 describe("UserService", () => {
-  let service: UserService;
+  let userService: UserService;
+  let userRepository: MockUserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService, { provide: getRepositoryToken(UserRepository), useValue: mockUserRepository() }],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get<MockUserRepository>(getRepositoryToken(UserRepository));
   });
 
   it("should be defined", () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
 });
