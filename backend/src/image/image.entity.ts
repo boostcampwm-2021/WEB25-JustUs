290df4a56d28bb3e16ee3673b61aa5ec4f2f351f
Younghan <soosungp33@gmail.com>
2021-11-29 12:54:29 +0900

    Fix : 이미지 폴더 복구

---
+++ backend/src/image/image.entity.ts
@@ -0,0 +1,16 @@
+import { TimeStampEntity } from "src/custom/myBaseEntity/timestampEntity";
+import { Post } from "src/post/post.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "images" })
+export class Image extends TimeStampEntity {
+  @PrimaryGeneratedColumn()
+  imageId: number;
+
+  @Column({ type: "text" })
+  imageUrl: string;
+
+  @ManyToOne(() => Post, post => post.images, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "post_id" })
+  post: Post;
+}
