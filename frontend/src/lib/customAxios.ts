fad56e141591d3cc3fe5097284e74fff896cc65d
myoung <48939876+Jo-Myounghee@users.noreply.github.com>
2021-12-01 10:56:54 +0900

    삭제 모달 및 axios 수정, 모달 UI 정리 (#259)
    * Feat: 자주 사용하는 multipart/form-data 헤더 변수화
    
    #247
    
    * Feat: 사용하지 않는 요청 제거
    
    #247
    
    * Refactor: axios 요청 url과 params로 나누어 리팩토링
    
    url, params를 각각 변수화하여 요청
    
    #247
    
    * Feat: 앨범 삭제, 포스트 삭제 모달 설정
    
    하나의 모달을 switch, case 사용해서 구분
    
    #248
    
    * Feat: small modal(앨범 삭제, 포스트 삭제) 공통 UI 통일
    
    작은 모달 창 css 전역으로 빼서 동일한 UI 가지도록 수정
    
    #232
    
    * Feat: 공통 css 속성 추가 및 속성 수정
    
    회원정보, 그룹정보 변경 모달 창 css 속성 하나로 합치기 전 그룹 정보 변경 모달 css 속성에서 공통 속성 대체하고 속성 수정
    
    #232

--- frontend/src/lib/customAxios.ts
+++ frontend/src/lib/customAxios.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
+export const multiFormDataHeader = { headers: { "Content-Type": "multipart/form-data" } };
 export const customAxios: AxiosInstance = axios.create({
   baseURL: `${SERVER_URL}`,
   withCredentials: true,
