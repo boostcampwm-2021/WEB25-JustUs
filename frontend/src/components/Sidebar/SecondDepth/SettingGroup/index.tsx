c1e4b67b840f74726f51b935a158e0abfc5aae95
gofeel8 <gofeel8@hanmail.net>
2021-12-01 15:35:48 +0900

    Feat: 그룹 정보 모달 타이틀에서 그룹 이름 보이도록 수정

--- frontend/src/components/Sidebar/SecondDepth/SettingGroup/index.tsx
+++ frontend/src/components/Sidebar/SecondDepth/SettingGroup/index.tsx
@@ -24,7 +24,9 @@ const SettingGroup = () => {
 
   return (
     <SettingGroupWrapper>
-      <GroupName>{selectedGroup.groupName}</GroupName>
+      <GroupName>
+        <span>{selectedGroup.groupName}</span>
+      </GroupName>
 
       <InfoIconWrapper onClick={onClickShowGroupInfo}>
         <InfoSVG fill={nowTheme.MENUTEXT} />
@@ -50,12 +52,13 @@ const GroupName = styled.div`
   grid-column-start: 1;
   grid-column-end: 2;
   cursor: default;
-  padding-left: 2rem;
+  padding-left: 1rem;
   overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  display: flex;
-  align-items: center;
+  ${flexRowCenterAlign}
+  & span {
+    overflow: hidden;
+    white-space: nowrap;
+  }
 `;
 const InfoIconWrapper = styled.div`
   cursor: pointer;
