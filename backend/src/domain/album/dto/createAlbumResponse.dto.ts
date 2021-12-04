eb83e183bf9743b6f7d2b45e6b7ded2c0b8dac8f
Younghan <soosungp33@gmail.com>
2021-12-04 16:09:18 +0900

    Refactor : 앨범 응답 dto 수정

--- backend/src/domain/album/dto/createAlbumResponse.dto.ts
+++ backend/src/domain/album/dto/createAlbumResponse.dto.ts
@@ -1,18 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Exclude, Expose } from "class-transformer";
+import { Exclude } from "class-transformer";
 import { Album } from "../album.entity";
 
 export class CreateAlbumResponseDto {
-  @Exclude() private readonly _albumId: number;
+  @Exclude()
+  @ApiProperty()
+  private readonly _albumId: number;
 
   constructor(album: Album) {
     this._albumId = album.albumId;
-  }
-
-  @ApiProperty()
-  @Expose()
-  get albumId(): number {
-    return this._albumId;
   }
 
   static returnDto(album: Album) {
